be0399034758cc2312193351b1216e2055d49135
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-06-10 15:51:00 +0200

    web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,8 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Mike'
-    fill_in :player_2_name, with: 'Arch Nemesis'
-    click_button 'Submit'
-    save_and_open_page
-    expect(page).to have_content 'Mike vs. Arch Nemesis'
+    sign_in_and_play
+    # save_and_open_page
+    expect(page).to have_content "Mike vs. Arch Nemesis"
     
   end
 end
